9c9a74c0093e3fc1d3d644d9e82b20230a2ed3c2
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2020-12-09 00:02:17 -0500

    Added Javadoc.

--- src/communicators/ConsoleCommunicator.java
+++ src/communicators/ConsoleCommunicator.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 /**
  * @author Eliezer Meth
  * Start Date: 2020-12-08
+ * Last Modified: 2020-12-09
  *
  * Class to get input from console.
  */
@@ -16,17 +17,28 @@ public class ConsoleCommunicator implements Communicator
     // Scanner to read from console
     private Scanner input;
 
+    /**
+     * Constructor.  Creates new Scanner.
+     */
     public ConsoleCommunicator()
     {
         input = new Scanner(System.in);
     }
 
+    /**
+     * Gets next line from console.
+     * @return String.
+     */
     @Override
     public String nextLine()
     {
         return input.nextLine();
     }
 
+    /**
+     * Prints string to console.
+     * @param str String.
+     */
     @Override
     public void send(String str)
     {
