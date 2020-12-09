ddc7adcc21805b144cc84cf95ff27b7e473216c4
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2020-12-08 23:28:25 -0500

    Added model interface and class for a constructed military model.

---
+++ src/Model.java
@@ -0,0 +1,16 @@
+/**
+ * @author Eliezer Meth
+ * Start Date: 2020-12-08
+ *
+ * Interface for containing a constructed model of the Enigma machine.
+ */
+
+public interface Model
+{
+    /**
+     * Method to type a letter into the Enigma machine.
+     * @param plaintextLetter Character to type into Enigma.
+     * @return Output letter of the Enigma.
+     */
+    char type(char plaintextLetter);
+}
